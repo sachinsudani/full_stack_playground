b975b6341a68b35e10d4deec6400a363bc03f52b
Sachin Sudani <sachinsudani60@gmail.com>
2025-04-21 23:25:50 +0530

    learn: operators

---
+++ js_foundation/part_1/logical.js
@@ -0,0 +1,14 @@
+/*  && -> AND
+    || -> OR
+    ! -> NOT
+*/
+
+let isLoggedIn = true;
+let isPaid = true;
+
+console.log(isLoggedIn && isPaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser);
---
+++ js_foundation/part_1/operation.js
@@ -0,0 +1,31 @@
+// Mathematical Operation
+
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let multiply = 3 * 5;
+let division = 8 / 2;
+
+let reminder = 9 % 2;
+let expo = 2 ** 3;
+
+let myScore = 110;
+myScore++;
+
+let credits;
+credits--;
+
+// Comparition Operation
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
