ebae42e41f3aa10b2110ec34bea46efee01f8c0f
Sachin Sudani <sachinsudani60@gmail.com>
2025-04-21 21:31:43 +0530

    learn: printing

---
+++ js_foundation/part_1/printing.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+console.log("Hello");
+
+process.stdout.write("Chai");
+process.stdout.write("Chai");
+
+console.table({ city: "Ahmedabad" });
+console.warn({ city: "Ahmedabad" });
