c827e58d0ec55050bd5361e9872c728fc62d6a13
Sachin Sudani <sachinsudani60@gmail.com>
2025-04-21 22:07:01 +0530

    learn: datatypes -> let, const and var

---
+++ js_foundation/part_1/change.js
@@ -0,0 +1,10 @@
+let gameName = "spiderman";
+
+gameName = "batman";
+
+console.log(gameName);
+
+const userName = "Hitesh";
+userName = "Chaudhary";
+
+console.log(userName);
---
+++ js_foundation/part_1/datatype.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+// console.log("Chai");
+
+/*
+Primitive Types
+    String
+    Number
+    Boolean
+    BigInt
+
+    Null
+    Undefined
+
+    Symbol
+
+Non_Primitive Types
+    Object
+*/
+
+// var score = 101;
+
+let score = 102;
+let name = "chaicode.com";
+let isLoggedIn = false;
+
+//object
+let teaTypes = ["lemon tea", "orange tea", "black tea"];
+
+let user = { firsName: "Hitesh", lastName: "Chaudhary" };
+
+let getScore = score;
+
+console.log(getScore);
