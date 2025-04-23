53ba51a3c226699ed2d1080f912f6e8eef6da374
Sachin Sudani <sachinsudani60@gmail.com>
2025-04-23 08:16:45 +0530

    learn: non-primitive data types

---
+++ js_foundation/part_2/non_primitive.js
@@ -0,0 +1,20 @@
+const user = {
+  firstName: "Hitesh",
+  isLoggedIn: true,
+};
+
+user.firstName = "Mr. H";
+user.lastName = "Chaudhary";
+
+// console.log(user);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+
+// let user = ["Hitesh", "Chaudhary", true];
+
+console.log(1 + "1");
+console.log("5" - 2);
+console.log(5 - "2");
