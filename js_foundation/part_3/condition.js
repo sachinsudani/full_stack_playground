08000efbeb310fa45d785bc432cecb030dad2315
Sachin Sudani <sachinsudani60@gmail.com>
2025-04-28 21:47:11 +0530

    learn: condition

---
+++ js_foundation/part_3/condition.js
@@ -0,0 +1,51 @@
+// Check if a number is greater than another number
+
+let num1 = 5;
+let num2 = 8;
+
+if (num1 > num2) {
+  // console.log("Num1 is gretaer than num2");
+} else {
+  // console.log("Nope, Num1 is NOT greater");
+}
+
+// Check if a string is equal to another string
+
+let username = "chai";
+let anotherUsername = "chai";
+
+if (username == anotherUsername) {
+  // console.log("Pick another username");
+} else {
+  // console.log("You can pick this username");
+}
+
+// Check if a variable is number or not
+
+let score = 44;
+
+if (typeof score == "number") {
+  // console.log("Yup, this is a number.");
+} else {
+  // console.log("No that is not a number.");
+}
+
+// Check if a boolean value is true or false
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+  // console.log("Tea is Ready");
+} else {
+  // console.log("Tea is NOT Ready");
+}
+
+// Check if an array is empty or not
+
+let items = [1];
+
+if (items.length) {
+  console.log("Array is not Empty.");
+} else {
+  console.log("Array is Empty");
+}
